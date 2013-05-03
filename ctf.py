0a5aab2c8b563a4b6cf553bb9aacd6ab0694508f
Brian J. McManus <bmcmanus@gmail.com>
2013-05-02 22:22:25 -0600

    Add /api/player/create resource

--- ctf.py
+++ ctf.py
@@ -1,24 +1,46 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from sqlalchemy import *
 import config
 from model import *
 import json
+import os
 
 app = Flask(__name__)
-
 app.debug = True
+# curl -H "Content-Type:application/json" http://localhost:5000/api/player/create -d '{"name":"Team Name/Persons Name"}'
 
+# curl -s -X POST -H "Content-Type:application/json" http://localhost:5000/api/player/create -d '{"name":"Team Aalphaaaaa"}'
+@app.route("/api/player/create", methods=['GET', 'POST'])
+def post_player():
+    db.session.rollback()
+    db.session.flush()
+
+    if request.method == 'GET':
+        return(json.dumps({'error': 'I... GET what you did but something isn\'t quite right here. '}))
+    elif request.method == 'POST':
+        token = os.urandom(16).encode('hex')
+        try:
+            db.session.add(Player(request.json['username'] if 'username' in request.json else u'', request.json['name'] if request.json['name'] else u'', u'', token, 1, 0))
+        except exc.IntegrityError:
+            db.session.rollback()
+            db.session.flush()
+            return(json.dumps({'error': 'Mmm, sorry. Your name isn\'t unique.'}))
+        db.session.commit()
+    return(json.dumps({"token": token}))
+#    return json.dumps(request.json['username'])
+    
 #Router
 @app.route("/api/board")
 def get_board_list():
-    return "%s" % json.dumps(Board.query.all(), default=ctf_model)
+    return json.dumps(Board.query.all(), default=ctf_model)
 
 @app.route("/api/player/<int:in_id>")
 def get_player_by_id(in_id):
-	return "%s" % json.dumps(Player.query.filter_by(id=in_id).all(), default=ctf_model)
+    return json.dumps(Player.query.filter_by(id=in_id).all(), default=ctf_model)
 
 @app.route("/api/player/<string:in_username>")
 def get_player_by_username(in_username):
-	return "%s" % json.dumps(Player.query.filter_by(username=in_username).all(), default=ctf_model)
+    return json.dumps(Player.query.filter_by(username=in_username).all(), default=ctf_model)
 
 @app.route("/")
 def hello():
@@ -34,9 +56,5 @@ if __name__ == "__main__":
     app.run(use_debugger=use_debugger, debug=app.debug,
             use_reloader=use_debugger, host='0.0.0.0')
     app.run()
-
-    
-    
     #python 
     #from model import db
-    
--- model.py
+++ model.py
@@ -3,42 +3,46 @@ import config
 import md5
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import Table, Column, Integer, String, Date, Float, Boolean, DateTime
+from sqlalchemy import Table, Column, Integer, String, Date, Float, Boolean, DateTime, exc
 # DB class
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =  config.DB_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
 db = SQLAlchemy(app)
-
+db.session.autocommit = True
+db.session.autoflush = True
 """
 Captures - the ledger for players who have Captured flags to earn points
 """
 class Capture(db.Model):
-	__tablename__ 	= 'captures'
-	id 				= Column(Integer, primary_key=True)
-	player_id 		= Column(Integer, db.ForeignKey('players.id'))
-	flag 			= Column(Integer, db.ForeignKey('flags.id'))
-	points_earned 	= Column(Integer)
-	created_at 		= Column(DateTime, default=db.func.now())
-	updated_at 		= Column(DateTime, default=db.func.now(), onupdate=db.func.now())
+    __tablename__ 	 = 'captures'
+    id 				 = Column(Integer, primary_key=True)
+    player_id 		 = Column(Integer, db.ForeignKey('players.id'))
+    flag 			 = Column(Integer, db.ForeignKey('flags.id'))
+    points_earned 	 = Column(Integer)
+    created_at 		 = Column(DateTime, default=db.func.now())
+    updated_at 		 = Column(DateTime, default=db.func.now(), onupdate=db.func.now())
 
 """
 Player engaged in the contest
 """
 class Player(db.Model):
-    __tablename__ 	= 'players'
-    id 		   		= Column(Integer, primary_key=True)
-    username   		= Column(String(60), unique=True)
-    email 	   		= Column(String(120), unique=True)
-    token 	   		= Column(String(160), unique=True)
-    board 	   		= db.relationship('Board', primaryjoin='(Player.id==Board.player_id)', backref='players')
-    active     		= Column(Boolean())
-    admin 	   		= Column(Boolean())
-    created_at 		= Column(DateTime, default=db.func.now())
-    updated_at 		= Column(DateTime, default=db.func.now(), onupdate=db.func.now())
+    __tablename__ 	 = 'players'
+    id 		   		 = Column(Integer, primary_key=True)
+    username   		 = Column(String(60), unique=False)
+    name			 = Column(String(60), unique=True)
+    email 	   		 = Column(String(120), unique=False)
+    token 	   		 = Column(String(160), unique=True)
+    board 	   		 = db.relationship('Board', primaryjoin='(Player.id==Board.player_id)', backref='players')
+    active     		 = Column(Boolean())
+    admin 	   		 = Column(Boolean())
+    created_at 		 = Column(DateTime, default=db.func.now())
+    updated_at 		 = Column(DateTime, default=db.func.now(), onupdate=db.func.now())
 
-    def __init__(self, username, email, token, active, admin):
-    	self.username = username
+    def __init__(self, username, name, email, token, active, admin):
+        self.username = username
+        self.name = name
         self.email = email
+        self.token = token
         self.admin = admin
         self.active = active
         
@@ -53,38 +57,38 @@ class Player(db.Model):
 Current board and stack-rank (may be unneeded)
 """
 class Board(db.Model):
-    __tablename__ 	= 'board'
-    id 				= Column(Integer, primary_key=True)
-    player_id 		= Column(Integer, db.ForeignKey('players.id'))
-    created_at 		= Column(DateTime, default=db.func.now())
-    updated_at 		= Column(DateTime, default=db.func.now(), onupdate=db.func.now())
+    __tablename__ 	 = 'board'
+    id 				 = Column(Integer, primary_key=True)
+    player_id 		 = Column(Integer, db.ForeignKey('players.id'))
+    created_at 		 = Column(DateTime, default=db.func.now())
+    updated_at 		 = Column(DateTime, default=db.func.now(), onupdate=db.func.now())
 
     def __init__(self, player_id):
-    	self.player_id = player_id
+        self.player_id = player_id
 
 """
 CTF Flags and their associated points (value)
 """
 class Flag(db.Model):
-    __tablename__ 	= 'flags'
-    id 				= Column(Integer, primary_key=True)
-    title 			= Column(String(60))
-    description 	= Column(String(512))
-    value 			= Column(Integer, default=0)
-    created_at 		= Column(DateTime, default=db.func.now())
-    updated_at 		= Column(DateTime, default=db.func.now(), onupdate=db.func.now())
+    __tablename__ 	 = 'flags'
+    id 				 = Column(Integer, primary_key=True)
+    title 			 = Column(String(60))
+    description 	 = Column(String(512))
+    value 			 = Column(Integer, default=0)
+    created_at 		 = Column(DateTime, default=db.func.now())
+    updated_at 		 = Column(DateTime, default=db.func.now(), onupdate=db.func.now())
     def __init__(self, title, description, value):
-    	self.title = title
-    	self.description = description
-    	self.value = value
+        self.title = title
+        self.description = description
+        self.value = value
 
 """
 Allows simplejson to sort our Models
 """
 def ctf_model(obj):
-	if isinstance(obj, Board):
-		return {'id': obj.id, 'player_id': obj.player_id, 'created_at': unicode(obj.created_at), 'updated_at':unicode(obj.updated_at)}
-	elif isinstance(obj, Player):
-		return {'id': obj.id, 'username': obj.username, 'email': obj.email, 'token': obj.token, 'board': obj.board, 'active': obj.active, 'created_at': unicode(obj.created_at), 'updated_at':unicode(obj.updated_at)}
-	else:
-		raise TypeError(repr(obj) + " is not JSON serializable")
+    if isinstance(obj, Board):
+        return {'id': obj.id, 'player_id': obj.player_id, 'created_at': unicode(obj.created_at), 'updated_at':unicode(obj.updated_at)}
+    elif isinstance(obj, Player):
+        return {'id': obj.id, 'username': obj.username, 'email': obj.email, 'token': obj.token, 'board': obj.board, 'active': obj.active, 'created_at': unicode(obj.created_at), 'updated_at':unicode(obj.updated_at)}
+    else:
+        raise TypeError(repr(obj) + " is not JSON serializable")
